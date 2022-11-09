1398bc28cb0747651acd1bddff50a2d37d70de16
sebas006 <seba_052@hotmail.com>
2022-11-08 22:10:34 -0300

    homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,6 +1,6 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
-// Crea una variable "string", puede contener lo que quieras:
+   // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
@@ -73,7 +73,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  return str1.length == str2.length  
+  return str1.length == str2.length;   
   
 }
 
